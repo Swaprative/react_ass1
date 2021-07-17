566a7706e05b38b0cb6eb633d71c2713ea3e4636
Swaprative <swaprativepla627@gmail.com>
2021-07-17 13:55:12 +0530

    tictac_ass2

---
+++ OneDrive/Desktop/Web/React_masterclass/tik-tak-toe-2/src/Components/Icon.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { FaTimes,FaRegCircle } from "react-icons/fa";
+
+const Icon=({choice})=>{
+            switch(choice)
+            {
+                  case "Cross":
+                        return <FaTimes className='icon'/>;
+                  case "Circle":
+                        return <FaRegCircle className='icon'/>;
+                  default :
+                        return " ";
+            }
+}
+
+export default Icon
